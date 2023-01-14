4a13e8b4e818d858b913bc4b1e95a95a5c2be3a3
MaksymKordyiaka <maxkord2003@gmail.com>
2023-01-14 11:48:37 +0200

    First commit

---
+++ main.py
@@ -0,0 +1,12 @@
+print("Hello, python!")
+
+a = str(input("enter a: "))
+b = str(a)
+a = int(b)
+c = id(b)
+print(c)
+
+name = str(input("Як тебе звати? "))
+print("Привіт " + name)
+age = str(input("Твій вік: "))
+print("Тобі зараз " + age + " років")
